a7f82ce995fc43d7b10baf5ba1358bc042b4a350
khushi <khusbumandal2060@gmail.com>
2026-06-24 01:26:49 +0545

    updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -164,7 +164,7 @@ export default function Hero() {
           <div className="h-px w-12 bg-zinc-800" />
           {[
             { icon: FaGithub, href: "https://github.com/Khushbumandal2060" },
-            { icon: FaLinkedin, href: "https://www.linkedin.com/in/khushbu-kumari-mandal-99637b343/" },
+            { icon: FaLinkedin, href: "https://www.linkedin.com/in/khushbu-kumari-mandal/" },
             { icon: FaInstagram, href: "https://www.instagram.com/khushbumandal15/" },
           ].map(({ icon: Icon, href }) => (
             <Link
